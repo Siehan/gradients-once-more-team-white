e6b2c325a90ed0e0c62c57cb1e6c7ef341d9abb7
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-06-02 01:30:23 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { GradientContextProvider } from "./context/GradientsContext";
 import GradientPage from "./page/GradientPage";
+import { Error404Page } from "./page/Error404Page";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,6 +15,8 @@ ReactDOM.render(
         <Switch>
           <Route exact path="/gradient/:id" component={GradientPage} />
           <Route exact path="/" component={App} />
+          <Route path="*" component={Error404Page} />
+          <Redirect to="/404" />
           <App />
         </Switch>
       </Router>
