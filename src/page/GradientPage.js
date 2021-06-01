f22ee7ee3e0a6ebd8e454eb94cdff86ec4b8831b
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-06-01 16:44:45 +0200

    Update GradientPage.js

--- src/page/GradientPage.js
+++ src/page/GradientPage.js
@@ -1,72 +1,73 @@
-import React from "react"
-import { NavLink, useParams } from "react-router-dom"
-import { useGradient } from "../context/GradientsContext"
-import GradientText from "./GradientText"
-import Footer from "../components/Footer"
-import GradientPageError from "./GradientPageError"
+import React from "react";
+import { NavLink, useParams } from "react-router-dom";
+import { useGradient } from "../context/GradientsContext";
+import GradientText from "./GradientText";
+import Footer from "../components/Footer";
+import GradientPageError from "./GradientPageError";
 
 const GradientPage = () => {
-	const { gradients } = useGradient()
-	const { id } = useParams()
+  const { gradients } = useGradient();
+  const { id } = useParams();
 
-	return (
-		<>
-			{id < 26 ? (
-				<div className="min-vh-100 d-flex flex-column">
-					<div
-						className="flex-fill d-flex"
-						style={{
-							backgroundImage: `linear-gradient(to right, ${
-								gradients[id - 1]?.start
-							}, ${gradients[id - 1]?.end})`,
-						}}
-					>
-						<nav className="fixed-top nav">
-							<li className="nav-item">
-								<NavLink
-									to="/"
-									type="button"
-									className="btn btn-dark text-white nav-link me-2"
-								>
-									Home
-								</NavLink>
-							</li>
-							{id > 1 ? (
-								<li className="nav-item">
-									<NavLink
-										to={`/gradient/${Number(id) - 1}`}
-										type="button"
-										className="btn btn-dark text-white nav-link me-2"
-									>
-										Previous
-									</NavLink>
-								</li>
-							) : (
-								""
-							)}
-							{id < 25 ? (
-								<li className="nav-item">
-									<NavLink
-										to={`/gradient/${Number(id) + 1}`}
-										type="button"
-										className="btn btn-dark text-white nav-link me-2"
-									>
-										Next
-									</NavLink>
-								</li>
-							) : (
-								""
-							)}
-						</nav>
-						<GradientText />
-					</div>
-					<Footer />
-				</div>
-			) : (
-				<GradientPageError />
-			)}
-		</>
-	)
-}
+  return (
+    <>
+      {id < 26 ? (
+        <div className="min-vh-100 d-flex flex-column">
+          <div
+            className="flex-fill d-flex"
+            style={{
+              backgroundImage: `linear-gradient(to right, ${gradients[id - 1]?.start}, ${gradients[id - 1]?.end})`,
+            }}
+          >
+            <nav className="fixed-top nav">
+              <li className="nav-item">
+                <NavLink
+                  to="/"
+                  type="button"
+                  aria-label="Click to go back to all the gradients"
+                  className="btn btn-dark text-white nav-link me-2"
+                >
+                  Home
+                </NavLink>
+              </li>
+              {id > 1 ? (
+                <li className="nav-item">
+                  <NavLink
+                    to={`/gradient/${Number(id) - 1}`}
+                    type="button"
+                    aria-label="Click to display the previous gradient"
+                    className="btn btn-dark text-white nav-link me-2"
+                  >
+                    Previous
+                  </NavLink>
+                </li>
+              ) : (
+                ""
+              )}
+              {id < 25 ? (
+                <li className="nav-item">
+                  <NavLink
+                    to={`/gradient/${Number(id) + 1}`}
+                    aria-label="Click to display the next gradient"
+                    type="button"
+                    className="btn btn-dark text-white nav-link me-2"
+                  >
+                    Next
+                  </NavLink>
+                </li>
+              ) : (
+                ""
+              )}
+            </nav>
+            <GradientText />
+          </div>
+          <Footer />
+        </div>
+      ) : (
+        <GradientPageError />
+      )}
+    </>
+  );
+};
 
-export default GradientPage
+export default GradientPage;
