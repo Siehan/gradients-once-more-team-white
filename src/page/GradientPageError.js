bac4806d2e25a4d9b3b28c6d1833f8c10faf51d6
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-06-01 21:00:38 +0200

    Update GradientPageError.js

--- src/page/GradientPageError.js
+++ src/page/GradientPageError.js
@@ -1,61 +1,62 @@
-import React from "react"
-import { NavLink, useParams } from "react-router-dom"
-import Footer from "../components/Footer"
+import React from "react";
+import { NavLink, useParams } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const GradientPageError = () => {
-	const { id } = useParams()
-	return (
-		<div className="min-vh-100 d-flex flex-column">
-			<div
-				className="flex-fill d-flex"
-				style={{
-					backgroundColor: "black",
-				}}
-			>
-				<nav className="fixed-top nav">
-					<li className="nav-item">
-						<NavLink
-							to="/"
-							type="button"
-							className="btn btn-dark text-white nav-link me-2"
-						>
-							Home
-						</NavLink>
-					</li>
-					{id > 1 ? (
-						<li className="nav-item">
-							<NavLink
-								to={`/gradient/${Number(id) - 1}`}
-								type="button"
-								className="btn btn-dark text-white nav-link me-2"
-							>
-								Previous
-							</NavLink>
-						</li>
-					) : (
-						""
-					)}
-					{id < 25 ? (
-						<li className="nav-item">
-							<NavLink
-								to={`/gradient/${Number(id) + 1}`}
-								type="button"
-								className="btn btn-dark text-white nav-link me-2"
-							>
-								Next
-							</NavLink>
-						</li>
-					) : (
-						""
-					)}
-				</nav>
-				<p className="text-white m-auto text-center">
-					Sorry, that gradient doesn't exist
-				</p>
-			</div>
-			<Footer />
-		</div>
-	)
-}
+  const { id } = useParams();
+  return (
+    <div className="min-vh-100 d-flex flex-column">
+      <div
+        className="flex-fill d-flex"
+        style={{
+          backgroundColor: "black",
+        }}
+      >
+        <nav className="fixed-top nav">
+          <li className="nav-item">
+            <NavLink
+              to="/"
+              type="button"
+              aria-label="Click to go back to all the gradients"
+              className="btn btn-dark text-white nav-link me-2"
+            >
+              Home
+            </NavLink>
+          </li>
+          {id > 1 ? (
+            <li className="nav-item">
+              <NavLink
+                to={`/gradient/${Number(id) - 1}`}
+                type="button"
+                aria-label="Click to display the previous gradient"
+                className="btn btn-dark text-white nav-link me-2"
+              >
+                Previous
+              </NavLink>
+            </li>
+          ) : (
+            ""
+          )}
+          {id < 25 ? (
+            <li className="nav-item">
+              <NavLink
+                to={`/gradient/${Number(id) + 1}`}
+                type="button"
+                aria-label="Click to display the next gradient"
+                className="btn btn-dark text-white nav-link me-2"
+              >
+                Next
+              </NavLink>
+            </li>
+          ) : (
+            ""
+          )}
+        </nav>
+        <p className="text-white m-auto text-center">Oops, this gradient does not exist</p>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-export default GradientPageError
+export default GradientPageError;
