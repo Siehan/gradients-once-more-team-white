bcc70c5ab615c910241626fce2e5b92df81f0eec
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-06-01 15:36:43 +0200

    Update GradientTagButton.js

--- src/components/GradientTagButton.js
+++ src/components/GradientTagButton.js
@@ -1,21 +1,22 @@
-import { useGradient } from "../context/GradientsContext"
+import { useGradient } from "../context/GradientsContext";
 
 const GradientTagButton = ({ tag }) => {
-	const { filter, setFilter } = useGradient()
+  const { filter, setFilter } = useGradient();
 
-	const className = filter === tag ? "bg-light" : "bg-dark text-white"
-	return (
-		<>
-			<button
-				type="button"
-				className={`btn btn-sm me-2 mb-2 ${className}`}
-				disabled={filter === tag}
-				onClick={() => setFilter(tag)}
-			>
-				{tag}
-			</button>
-		</>
-	)
-}
+  const className = filter === tag ? "bg-light" : "bg-dark text-white";
+  return (
+    <>
+      <button
+        type="button"
+        aria-label="Click to select the color"
+        className={`btn btn-sm me-2 mb-2 ${className}`}
+        disabled={filter === tag}
+        onClick={() => setFilter(tag)}
+      >
+        {tag}
+      </button>
+    </>
+  );
+};
 
-export default GradientTagButton
+export default GradientTagButton;
