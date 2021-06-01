4a9ee89a1aef461c9aac5e4b66d362aeb02eef76
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-06-01 14:58:02 +0200

    Update GradientButton.js

--- src/components/GradientButton.js
+++ src/components/GradientButton.js
@@ -1,14 +1,16 @@
-import React from "react"
-import { NavLink } from "react-router-dom"
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const GradientButton = ({ id }) => {
-	return (
-		<div className="mt-3">
-			<NavLink to={`/gradient/${id}`}>
-				<button className="btn btn-outline-dark w-100">Full screen</button>
-			</NavLink>
-		</div>
-	)
-}
+  return (
+    <div className="mt-3">
+      <NavLink to={`/gradient/${id}`}>
+        <button aria-label="Click to display the full screen" className="btn btn-outline-dark w-100">
+          Full screen
+        </button>
+      </NavLink>
+    </div>
+  );
+};
 
-export default GradientButton
+export default GradientButton;
