2a646faeb7fd826921243ffa5b65f9445a30da49
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-06-03 01:31:32 +0200

    Update Gradients.js

--- src/components/Gradients.js
+++ src/components/Gradients.js
@@ -1,21 +1,40 @@
-import GradientsList from "./GradientsList"
-import GradientsSelect from "./GradientsSelect"
-import { useGradient } from "../context/GradientsContext"
+import GradientsList from "./GradientsList";
+import GradientsSelect from "./GradientsSelect";
+import { useGradient } from "../context/GradientsContext";
 
 const Gradients = () => {
-	const { loading, error } = useGradient()
+  const { loading, error } = useGradient();
 
-	return (
-		<div>
-			{loading ? (
-				<p className="text-center">Loading...</p>
-			) : (
-				<GradientsSelect />
-			)}
-			{error && <p>{error}</p>}
-			<GradientsList />
-		</div>
-	)
-}
+  return (
+    <div>
+      {loading ? (
+        <p className="text-center">
+          <div className="spinner-border text-primary" role="status">
+            <span className="sr-only"></span>
+          </div>
+          <div className="spinner-border text-success" role="status">
+            <span className="sr-only"></span>
+          </div>
+          <div className="spinner-border text-danger" role="status">
+            <span className="sr-only"></span>
+          </div>
+          <div className="spinner-border text-warning" role="status">
+            <span className="sr-only"></span>
+          </div>
+          <div className="spinner-border text-info" role="status">
+            <span className="sr-only"></span>
+          </div>
+          <div className="spinner-border text-dark" role="status">
+            <span className="sr-only"></span>
+          </div>
+        </p>
+      ) : (
+        <GradientsSelect />
+      )}
+      {error && <p>{error}</p>}
+      <GradientsList />
+    </div>
+  );
+};
 
-export default Gradients
+export default Gradients;
