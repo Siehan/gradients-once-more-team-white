130f8a17a1b1183c68f4a213727695506bb01b06
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-06-02 22:12:33 +0200

    Update SwitchPage.js

--- src/page/SwitchPage.js
+++ src/page/SwitchPage.js
@@ -5,25 +5,30 @@ const SwitchPage = () => {
   const { page, setPage } = useGradient();
   return (
     <div className="btn-group mb-4">
-      <button aria-label="" className="btn btn-outline-dark" disabled={page === "tous"} onClick={() => setPage("tous")}>
+      <button
+        aria-label="Click to show all"
+        className="btn btn-outline-light rounded-3 m-1"
+        disabled={page === "tous"}
+        onClick={() => setPage("tous")}
+      >
         Tous
       </button>
-      <button className="btn btn-outline-dark" disabled={page === 1} onClick={() => setPage(1)}>
+      <button className="btn btn-outline-light rounded-3 m-1" disabled={page === 1} onClick={() => setPage(1)}>
         1
       </button>
-      <button className="btn btn-outline-dark" disabled={page === 2} onClick={() => setPage(2)}>
+      <button className="btn btn-outline-light rounded-3 m-1" disabled={page === 2} onClick={() => setPage(2)}>
         2
       </button>
-      <button className="btn btn-outline-dark" disabled={page === 3} onClick={() => setPage(3)}>
+      <button className="btn btn-outline-light rounded-3 m-1" disabled={page === 3} onClick={() => setPage(3)}>
         3
       </button>
-      <button className="btn btn-outline-dark" disabled={page === 4} onClick={() => setPage(4)}>
+      <button className="btn btn-outline-light rounded-3 m-1" disabled={page === 4} onClick={() => setPage(4)}>
         4
       </button>
-      <button className="btn btn-outline-dark" disabled={page === 5} onClick={() => setPage(5)}>
+      <button className="btn btn-outline-light rounded-3 m-1" disabled={page === 5} onClick={() => setPage(5)}>
         5
       </button>
-      <button className="btn btn-outline-dark" disabled={page === 6} onClick={() => setPage(6)}>
+      <button className="btn btn-outline-light rounded-3 m-1" disabled={page === 6} onClick={() => setPage(6)}>
         6
       </button>
     </div>
