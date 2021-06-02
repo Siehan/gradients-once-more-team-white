4797c51f3fc5bce468d3e99a384175c9314cfcee
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-06-02 21:34:17 +0200

    Update ToggleModeButton.js

--- src/components/ToggleModeButton.js
+++ src/components/ToggleModeButton.js
@@ -1,30 +1,30 @@
 const ToggleModeButton = (props) => {
-	const { darkMode, setDarkMode } = props
+  const { darkMode, setDarkMode } = props;
 
-	const handleButtonClick = () => {
-		setDarkMode(!darkMode)
-	}
+  const handleButtonClick = () => {
+    setDarkMode(!darkMode);
+  };
 
-	return (
-		<>
-			{!darkMode ? (
-				<button
-					onClick={handleButtonClick}
-					aria-label="Clickez pour afficher le mode sombre"
-					className="btn btn-dark position-absolute"
-				>
-					🌒
-				</button>
-			) : (
-				<button
-					onClick={handleButtonClick}
-					aria-label="Clickez pour afficher le mode clair"
-					className="btn btn-light position-absolute"
-				>
-					🌔
-				</button>
-			)}
-		</>
-	)
-}
-export default ToggleModeButton
+  return (
+    <>
+      {!darkMode ? (
+        <button
+          onClick={handleButtonClick}
+          aria-label="Clickez pour afficher le mode sombre"
+          className="btn btn-dark position-absolute"
+        >
+          🌒
+        </button>
+      ) : (
+        <button
+          onClick={handleButtonClick}
+          aria-label="Clickez pour afficher le mode couleur"
+          className="btn btn-warning position-absolute"
+        >
+          🌔
+        </button>
+      )}
+    </>
+  );
+};
+export default ToggleModeButton;
