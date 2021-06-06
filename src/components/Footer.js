4459943c7b4b915563f7a3de4ae4360eec5a4dcb
Sylvie Mémain-Yé <53022990+Siehan@users.noreply.github.com>
2021-06-06 04:26:28 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,12 @@
 const Footer = () => {
-	return (
-		<footer className="text-center text-white bg-dark p-3 mt-auto">
-			Made with <span className="text-danger">&hearts;</span>
-			<span className="text-warning">&hearts;</span>
-			<span className="text-primary">&hearts;</span>
-			<span className="text-success">&hearts;</span> & React by Nassim, Grégory,
-			Victor & Sylvie for Alyra White Team
-		</footer>
-	)
-}
+  return (
+    <footer className="footer-background text-center text-white p-3 mt-auto">
+      Made with <span className="text-danger">&hearts;</span>
+      <span className="text-warning">&hearts;</span>
+      <span className="text-primary">&hearts;</span>
+      <span className="text-success">&hearts;</span> & React by Nassim, Grégory, Victor & Sylvie for Alyra White Team
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
